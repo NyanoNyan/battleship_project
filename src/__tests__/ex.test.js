eacfaae3aa7e35ad5ff52c0ee567700a20382e50
nyano <nayan100@hotmail.co.uk>
2021-03-16 08:49:55 +0000

    Modified the test run, added describe to split tests out

--- src/__tests__/ex.test.js
+++ src/__tests__/ex.test.js
@@ -1,4 +1,11 @@
-test("test",() => {
-    const i = "hello";
-    expect(i).toEqual("hello");
+const { GameBoard } = require("../try");
+
+
+describe("Create Gameboard", () => {
+    test("Check coord match", () => {
+        const GameBoardV = GameBoard();
+        console.log(GameBoardV.receiveAttack("A1"))
+        expect(GameBoardV.receiveAttack("A1")).toEqual([{"Cruiser": ["A1", "A2", "A3"]}]);
+    });
 })
+
