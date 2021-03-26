fd044986cd3ddc1dfa2e52ac034c69ea8d66199b
nyano <nayan100@hotmail.co.uk>
2021-03-26 09:21:12 +0000

    Setting up the game board display.

---
+++ src/components/DisplayBoard.js
@@ -0,0 +1,18 @@
+
+
+const DispalyBoard = () => {
+
+    const makeBoars = () => {
+
+    }
+
+
+    return (
+        <div>I'm Inside DisplayBoard</div>
+    )
+};
+
+export default DispalyBoard;
+
+
+// Material UI Grid?
