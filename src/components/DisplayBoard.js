41903fe8d09de0613ab69e69016161e8af59c9ea
nyano <nayan100@hotmail.co.uk>
2021-03-29 10:17:16 +0100

    Made a componenet which make a 100 square for a board of 10*10

--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -1,18 +1,37 @@
 
-
+import "../styles/DisplayBoard.css"
 const DispalyBoard = () => {
 
-    const makeBoars = () => {
+    let test = [];
 
+    for (let i = 0; i < 100; i++) {
+        test.push((
+            <Square />
+        ))
     }
 
-
     return (
-        <div>I'm Inside DisplayBoard</div>
+        <div>
+            <div>I'm Inside DisplayBoard</div>
+            <div className="main-board">
+                {test}
+            </div>
+        </div>
+
     )
 };
+
+const Square = () => {
+    return (
+        <button className="square">
+        </button>
+    )
+}
 
 export default DispalyBoard;
 
 
 // Material UI Grid?
+// How to make a grid using react, not repeatable using dom to insert elements etc. Is there a library?
+
+// But each ID inside square.
