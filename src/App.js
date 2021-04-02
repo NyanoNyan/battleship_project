cd44754864a4347881d9170dab56a087298c71d0
nyano <nayan100@hotmail.co.uk>
2021-04-02 09:31:58 +0100

    Added player move, fixed issue of not returning just one true of false if it's been hit. Added red as the background colour when hit and made sure the player cannot click the sqaure again by removing pointer events. Also added random computer move, same thing as the player, except extracted the element using DOM instead of onClick event. Added setUpBoard, to set up the board and to clean it up to make sure it's placed in a function. CheckWinner still needs work.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import DisplayBoard from "./components/DisplayBoard.js"
+import DisplayBoard from "./components/DisplayBoard.js";
 
 import Ship from "../src/factory_functions/ship";
 import GameBoard from "../src/factory_functions/gameboard";
 import Player from "../src/factory_functions/player";
 
-
 const App = () => {
-  const [isSetUp, setIsSetUp] = useState(false);
+    const [isSetUp, setIsSetUp] = useState(false);
+    const [winner, setWinner] = useState("");
+
+    useEffect(() => {
+      checkWinner();
+    }, [winner]);
+    // When a players selects a box to commence an attack.
+    const onClick = (e) => {
+        // Player Move
+        e.target.textContent = "X";
+        let isHitP = newGameBoardP.receiveAttack(e.target.id);
+
+        if (isHitP === true) {
+            e.target.style.background = "red";
+        }
+        e.target.style.pointerEvents = "none";
+
+        compMove();
+        console.log(newGameBoardP.checkAllSunk());
+    };
+
+    // Random Computer Move
+    const compMove = () => {
+        const compMove = newPlayers.computerMove();
+        const eleComp = document.querySelector(`#${compMove}`);
+        eleComp.textContent = "X";
+
+        if (newGameBoardC.receiveAttack(compMove) === true) {
+            eleComp.style.background = "red";
+        }
+        console.log(eleComp);
+    };
+
+    const checkWinner = () => {
+      if (newGameBoardP.checkAllSunk() === true) {
+        setWinner("Player wins!");
+      } else if (newGameBoardC.checkAllSunk() === true) {
+        setWinner("Computer wins!")
+      }
   
+    }
 
-  // Set up methods
-  const onClick = (e) => {
-    e.target.textContent = "X";
-    let test = newGameBoardP.receiveAttack(e.target.id);
-    console.log(test);
-    // console.log(newGameBoardP.checkAllSunk());
-  }
+    const setUpBoard = () => {
+        // Player Board Set up
+        newGameBoardP.placeShips("Carrier", ["A1", "A2", "A3", "A4", "A5"], 5);
+        newGameBoardP.placeShips("Battleship", ["C1", "C2", "C3", "C4"], 4);
+        newGameBoardP.placeShips("Cruiser", ["E1", "E2", "E3"], 3);
+        newGameBoardP.placeShips("Submarine", ["G1", "G2", "G3"], 3);
+        newGameBoardP.placeShips("Destroyer", ["I1", "I2"], 2);
+        // Comp Board Set up
+        newGameBoardC.placeShips("Carrier", ["J1", "J2", "J3", "J4", "J5"], 5);
+        newGameBoardC.placeShips("Battleship", ["H1", "H2", "H3", "H4"], 4);
+        newGameBoardC.placeShips("Cruiser", ["F1", "F2", "F3"], 3);
+        newGameBoardC.placeShips("Submarine", ["D1", "D2", "D3"], 3);
+        newGameBoardC.placeShips("Destroyer", ["B1", "B2"], 2);
+    };
 
+    // Set up Game
+    const newGameBoardP = GameBoard();
+    const newGameBoardC = GameBoard();
+    const newPlayers = Player();
+    setUpBoard();
 
-  // Set up Game
-  const newGameBoardP = GameBoard();
-  const newGameBoardC = GameBoard();
-  const newPlayers = Player();
-
-  newGameBoardP.placeShips("Carrier", ["A1", "A2", "A3", "A4", "A5"], 5);
-  newGameBoardP.placeShips("Battleship", ["C1", "C2", "C3", "C4"], 4);
-  newGameBoardP.placeShips("Cruiser", ["E1", "E2", "E3"], 3);
-  newGameBoardP.placeShips("Submarine", ["G1", "G2", "G3"], 3);
-  newGameBoardP.placeShips("Destroyer", ["I1", "I2"], 2);
-
-  newGameBoardC.placeShips("Carrier", ["J1", "J2", "J3", "J4", "J5"], 5);
-  newGameBoardC.placeShips("Battleship", ["H1", "H2", "H3", "H4"], 4);
-  newGameBoardC.placeShips("Cruiser", ["F1", "F2", "F3"], 3);
-  newGameBoardC.placeShips("Submarine", ["D1", "D2", "D3"], 3);
-  newGameBoardC.placeShips("Destroyer", ["B1", "B2"], 2);
-
-  return (
-    <div>
-      <DisplayBoard 
-        onClick = {onClick}
-      />
-    </div>
-  )
-}
+  
+    return (
+        <div>
+            <DisplayBoard onClick={onClick} />
+            <p> {console.log(winner)} </p>
+        </div>
+    );
+};
 
 export default App;
