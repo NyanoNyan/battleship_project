85a322831512d2ed89f1c9263004439d5481159e
nyano <nayan100@hotmail.co.uk>
2021-04-22 17:11:08 +0100

    Made making the Boards a little simpler than previously

--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -2,11 +2,49 @@ import "../styles/DisplayBoard.css";
 import createValidMoves from "../factory_functions/createValidMoves";
 
 const DisplayBoard = (props) => {
+    const { markCoord } = props;
     return (
         <div>
-            Hello
+            <h3> Battleship Project</h3>
+            <div className="main-board">
+                <div id="computer-board">
+                    <p className="board-heading">Computer Board</p>
+                    <div className="board-types"> 
+                        <CreateBoard 
+                            markCoord = {markCoord}
+                        />
+                    </div>
+                </div>
+
+                <div id="player-board">
+                    <p className="board-heading">Player Board</p>
+                    <div className="board-types">
+                        <CreateBoard 
+                            markCoord = {markCoord}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+
+    );
+};
+
+const CreateBoard = (props) => {
+    const { markCoord } = props;
+    const validMoves = createValidMoves("A", "J");
+    let boardP = []
+    for (let i = 0; i < 100; i++) {
+        boardP.push(
+        <button 
+            key= {i} 
+            id={validMoves[i]}
+            className="square square-bg"
+            onClick={(e) => markCoord(e)}>
+        </button>
+        );
+    };
+    return boardP;
 };
 
 export default DisplayBoard;
