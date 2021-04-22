66a5cf40cf7ab6704e00a5fe2a3bbe60d04a8bb6
nyano <nayan100@hotmail.co.uk>
2021-04-22 17:09:49 +0100

    Remade the app of the logic to be clearer, right now looking into resetting CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./styles/App.css";
 import DisplayBoard from "./components/DisplayBoard.js";
 
 import Ship from "../src/factory_functions/ship";
@@ -8,18 +8,60 @@ import Player from "../src/factory_functions/player";
 
 const App = () => {
 
+  const [isRestart, setIsRestart] = useState(false);
+  const [winner, setWinner] = useState("");
+
+  const setUpBoard = (gameBoardP, gameBoardC) => {
+    gameBoardP.placeShips("Carrier", ["A1", "A2", "A3", "A4", "A5"], 5);
+    gameBoardP.placeShips("Battleship", ["C1", "C2", "C3", "C4"], 4);
+    gameBoardP.placeShips("Cruiser", ["E1", "E2", "E3"], 3);
+    gameBoardP.placeShips("Submarine", ["G1", "G2", "G3"], 3);
+    gameBoardP.placeShips("Destroyer", ["I1", "I2"], 2);
+    // Comp Board Set up
+    gameBoardC.placeShips("Carrier", ["J1", "J2", "J3", "J4", "J5"], 5);
+    gameBoardC.placeShips("Battleship", ["H1", "H2", "H3", "H4"], 4);
+    gameBoardC.placeShips("Cruiser", ["F1", "F2", "F3"], 3);
+    gameBoardC.placeShips("Submarine", ["D1", "D2", "D3"], 3);
+    gameBoardC.placeShips("Destroyer", ["B1", "B2"], 2);
+  };
+
+  const restartBoard = (gameBoardP, gameBoardC) => {
+    gameBoardP = GameBoard();
+    gameBoardC = GameBoard();
+    setIsRestart(true);
+  }
+
+  const markCoord = (e) => {
+    if (e.target.className === "square square-bg") {
+      e.target.className = "square square-bg2"
+    }
+  }
+
+  // Set up the Board
+  let gameBoardP = GameBoard();
+  let gameBoardC = GameBoard();
+  setUpBoard(gameBoardP, gameBoardC);
+
+  // Testing hits
+  let tt = gameBoardP.showHits();
+  console.log(tt[0]);
+
   // Deals with setting up the game //
     return (
-      <DisplayBoard />
+      <div >
+            <DisplayBoard 
+              markCoord = {markCoord}
+            />
+            <p> {winner} </p>
+            <button onClick={restartBoard}>Restart Game?</button>
+      </div>
+
+
     )
 };
 
+
+
 export default App;
 
-// Things that needs to be completed
-// - Restart the game
-// - Random ships placements for both computer and player (optional)
-
-// learn more about useEffect
-// test why hitLocs is not showing
-// Look at other codes for inspiration
+// How to reset CSS, using hooks, remaking the componenets?
